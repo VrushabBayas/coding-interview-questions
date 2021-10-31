7f5fe9823f27fa09cd06ebaf0bb87909107c4b04
Vrushab Bayas <vrushab.bayas@gmail.com>
2021-10-31 12:13:58 +0530

    target sum code un commented

--- TargetSum/targetSum.js
+++ TargetSum/targetSum.js
@@ -1,14 +1,14 @@
 function findTheSum(array, targetNum) {
-  // const nums = {};
-  // for (const num of array) {
-  //   let temp = targetNum - num;
-  //   if (nums[temp]) {
-  //     return [temp, num];
-  //   } else {
-  //     nums[num] = true;
-  //   }
-  // }
-  // return [];
+  const nums = {};
+  for (const num of array) {
+    let temp = targetNum - num;
+    if (nums[temp]) {
+      return [temp, num];
+    } else {
+      nums[num] = true;
+    }
+  }
+  return [];
 }
 
 module.exports = findTheSum;
